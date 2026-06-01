db668470bf41d7e3864aa9442530db1e0f87e5db
Hugo (Aquele) Lopes <contato@aquelehugo.com>
2026-06-01 19:37:25 -0300

    feat: set orange for good enough pitch

--- app/core/updateLastPitch.js
+++ app/core/updateLastPitch.js
@@ -1,16 +1,29 @@
 import noteFrequencyTuples from '../constants/noteFrequencyTuples'
 import getLogPitchY from './getLogPitchY'
 
-const isNearNote = frequency =>
-  noteFrequencyTuples.some(noteFrequencyTuple => {
+const getPitchRating = frequency =>
+  noteFrequencyTuples.reduce((rating, noteFrequencyTuple) => {
     const [_, expectedFrequency] = noteFrequencyTuple
     // Calculate deviation in cents: C = 1200 * log2(f / f_0)
-    const ratio = frequency / expectedFrequency;
-    const deviationCents = 1200 * Math.log2(ratio);
+    const ratio = frequency / expectedFrequency
+    const absoluteDeviationCents = Math.abs(1200 * Math.log2(ratio))
 
-    // Check if absolute deviation is within 10 cents
-    return Math.abs(deviationCents) <= 10
-  })
+    if (absoluteDeviationCents <= 10) {
+      return 'perfect'
+    }
+
+    if (absoluteDeviationCents <= 25) {
+      return 'good'
+    }
+
+    return rating
+  }, 'bad')
+
+const COLORS_BY_RATING = {
+  perfect: 'green',
+  good: 'orange',
+  bad: 'red',
+}
 
 const updateLastPitch = (frequency, canvasWidth) => appContext => {
   const { lastPitch, pitchSize, pitchLines } = appContext
@@ -19,6 +32,8 @@ const updateLastPitch = (frequency, canvasWidth) => appContext => {
       ? pitchLines.offset.x
       : lastPitch.position.x + pitchSize.width
 
+  const rating = getPitchRating(frequency)
+
   return {
     ...appContext,
     lastPitch: {
@@ -26,7 +41,7 @@ const updateLastPitch = (frequency, canvasWidth) => appContext => {
         x: newPitchX > canvasWidth ? pitchLines.offset.x : newPitchX,
         y: getLogPitchY(frequency)(appContext) - pitchSize.height / 2,
       },
-      color: isNearNote(frequency) ? 'green' : 'red',
+      color: COLORS_BY_RATING[rating],
     },
   }
 }
